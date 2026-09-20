5dc0bb0c01fc2c3c4dc8465f6890f2ff24465cda
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-20 21:44:51 +0200

    feat: install client genesis payload handlers

--- src/main/java/io/github/artificialturtill/myriadascension/client/MyriadAscensionClient.java
+++ src/main/java/io/github/artificialturtill/myriadascension/client/MyriadAscensionClient.java
@@ -1,6 +1,7 @@
 package io.github.artificialturtill.myriadascension.client;
 
 import io.github.artificialturtill.myriadascension.MyriadAscension;
+import io.github.artificialturtill.myriadascension.network.ClientPayloadBridge;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -9,6 +10,10 @@ import net.neoforged.neoforge.common.NeoForge;
 @Mod(value = MyriadAscension.MOD_ID, dist = Dist.CLIENT)
 public final class MyriadAscensionClient {
     public MyriadAscensionClient(IEventBus modEventBus) {
+        ClientPayloadBridge.installGenesisHandlers(
+                ClientGenesisPayloadHandler::openGenesis,
+                ClientGenesisPayloadHandler::handleGenesisResult);
+
         modEventBus.addListener(ClientKeyMappings::register);
         NeoForge.EVENT_BUS.addListener(ClientCultivationInput::onClientTick);
     }
