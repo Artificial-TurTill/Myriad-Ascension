9234fd7577df36638bd2a619d7bfdf2489d2feee
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-20 21:44:53 +0200

    feat: request genesis on first player login

--- src/main/java/io/github/artificialturtill/myriadascension/cultivation/data/PlayerCultivationEvents.java
+++ src/main/java/io/github/artificialturtill/myriadascension/cultivation/data/PlayerCultivationEvents.java
@@ -1,9 +1,23 @@
 package io.github.artificialturtill.myriadascension.cultivation.data;
 
+import io.github.artificialturtill.myriadascension.network.OpenGenesisPayload;
+import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
 public final class PlayerCultivationEvents {
     private PlayerCultivationEvents() {
+    }
+
+    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!(event.getEntity() instanceof ServerPlayer player)) {
+            return;
+        }
+
+        CultivatorData data = player.getData(ModAttachments.CULTIVATOR_DATA);
+        if (!data.hasCompletedInitialSetup()) {
+            PacketDistributor.sendToPlayer(player, OpenGenesisPayload.INSTANCE);
+        }
     }
 
     public static void onPlayerClone(PlayerEvent.Clone event) {
