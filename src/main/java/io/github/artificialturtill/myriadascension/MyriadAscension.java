bda9c1322c408b5b88a2a32e56a211455c931e8e
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-20 20:34:03 +0200

    feat: register cultivator data attachment and death recovery

--- src/main/java/io/github/artificialturtill/myriadascension/MyriadAscension.java
+++ src/main/java/io/github/artificialturtill/myriadascension/MyriadAscension.java
@@ -1,9 +1,12 @@
 package io.github.artificialturtill.myriadascension;
 
 import com.mojang.logging.LogUtils;
+import io.github.artificialturtill.myriadascension.cultivation.data.ModAttachments;
+import io.github.artificialturtill.myriadascension.cultivation.data.PlayerCultivationEvents;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(MyriadAscension.MOD_ID)
@@ -12,6 +15,9 @@ public final class MyriadAscension {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public MyriadAscension(IEventBus modEventBus, ModContainer modContainer) {
+        ModAttachments.register(modEventBus);
+        NeoForge.EVENT_BUS.addListener(PlayerCultivationEvents::onPlayerClone);
+
         LOGGER.info("Myriad Ascension initializing on NeoForge.");
     }
 }
