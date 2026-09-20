0f503c8cf31b7769504d962ad04af4cf3f3af557
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 01:45:16 +0200

    feat: add Testudo clan institution rules (LibraryAccessResult.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/library/LibraryAccessResult.java
@@ -0,0 +1,10 @@
+package io.github.artificialturtill.myriadascension.library;
+
+public enum LibraryAccessResult {
+    AUTHORIZED,
+    NOT_A_MEMBER,
+    RANK_TOO_LOW,
+    NOT_SELECTED_BY_OVERSEER,
+    BOOK_NOT_FOUND,
+    TECHNIQUE_NOT_PERMITTED
+}
