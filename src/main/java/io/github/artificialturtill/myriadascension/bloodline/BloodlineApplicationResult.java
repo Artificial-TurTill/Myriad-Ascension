c0d32d5fe7b187d68809cf94e1f6f4975a598c4d
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 01:07:55 +0200

    feat: add tortoise bloodline foundation (BloodlineApplicationResult.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/bloodline/BloodlineApplicationResult.java
@@ -0,0 +1,8 @@
+package io.github.artificialturtill.myriadascension.bloodline;
+
+public enum BloodlineApplicationResult {
+    INSTALLED,
+    OVERWRITTEN_BY_STRONGER_SAME_LINEAGE,
+    REJECTED_SAME_OR_WEAKER_LINEAGE,
+    INCOMPATIBLE_LINEAGE_CONFLICT
+}
