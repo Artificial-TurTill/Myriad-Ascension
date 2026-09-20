5a2144eea0da10bf494ea8db6abbae0d57541c95
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-20 21:41:27 +0200

    feat: add social and Qi exposure rules (QiExposureType.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/cultivation/qi/QiExposureType.java
@@ -0,0 +1,11 @@
+package io.github.artificialturtill.myriadascension.cultivation.qi;
+
+public enum QiExposureType {
+    SUPPRESSED,
+    PASSIVE_AURA,
+    ACTIVE_CIRCULATION,
+    CALM_CULTIVATION,
+    WEAPON_INFUSION,
+    QI_TECHNIQUE,
+    BURST
+}
