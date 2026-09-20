872ac4dc2b840edee8cb6fc64e257c07fb9fe3e4
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 01:09:07 +0200

    feat: define Testudo bloodline benefits (BloodlineBenefitType.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/bloodline/BloodlineBenefitType.java
@@ -0,0 +1,11 @@
+package io.github.artificialturtill.myriadascension.bloodline;
+
+public enum BloodlineBenefitType {
+    LONGEVITY,
+    DEFENSE,
+    REGENERATION,
+    EARTH_COMPATIBILITY,
+    RESISTANCE,
+    SHELL_TECHNIQUE_COMPATIBILITY,
+    BREAKTHROUGH_STABILITY
+}
