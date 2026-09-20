710932afb1da15406694284d9cbe3e01b1023cdd
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 01:07:48 +0200

    feat: add tortoise bloodline foundation (BloodlineAcquisitionType.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/bloodline/BloodlineAcquisitionType.java
@@ -0,0 +1,14 @@
+package io.github.artificialturtill.myriadascension.bloodline;
+
+public enum BloodlineAcquisitionType {
+    QUEST,
+    ANCIENT_CULTIVATOR_TOMB,
+    MONUMENT,
+    AUCTION,
+
+    NPC_FAMILY_INHERITANCE,
+
+    SPIRIT_PACT,
+    BEAST_KILL,
+    NATURAL_LAST_BREATH
+}
