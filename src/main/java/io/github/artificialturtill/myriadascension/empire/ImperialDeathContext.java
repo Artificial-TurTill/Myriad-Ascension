a4af146575df538758d07528a11009ca6987faf5
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 00:03:15 +0200

    feat: define imperial politics foundation (ImperialDeathContext.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/empire/ImperialDeathContext.java
@@ -0,0 +1,14 @@
+package io.github.artificialturtill.myriadascension.empire;
+
+public enum ImperialDeathContext {
+    POLITICAL_BATTLE,
+    ENEMY_ATTACK,
+    NATURAL,
+    FALL,
+    FIRE,
+    LAVA,
+    DROWNING,
+    SUFFOCATION,
+    OTHER_ENVIRONMENTAL,
+    UNKNOWN
+}
