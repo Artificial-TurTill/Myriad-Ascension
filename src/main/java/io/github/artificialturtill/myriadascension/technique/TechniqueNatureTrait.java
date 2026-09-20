5d48a975394b6cecf202abb49451fc04ec253718
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 00:00:42 +0200

    feat: add cultivation progression foundations (TechniqueNatureTrait.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/technique/TechniqueNatureTrait.java
@@ -0,0 +1,16 @@
+package io.github.artificialturtill.myriadascension.technique;
+
+public enum TechniqueNatureTrait {
+    HARMONIZING,
+    VIOLENT,
+    POISON,
+    BLOOD,
+    LIFE_DRAIN,
+    GLUTTONY,
+    DESIRE,
+    SOUL,
+    BODY_TEMPERING,
+    LONGEVITY,
+    DEFENSIVE,
+    CONCEALMENT
+}
