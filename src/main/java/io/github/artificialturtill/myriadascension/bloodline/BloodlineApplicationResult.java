220976054da08eae76a1dceeb7861c9c60637934
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 01:09:55 +0200

    feat: support same-lineage bloodline purification

--- src/main/java/io/github/artificialturtill/myriadascension/bloodline/BloodlineApplicationResult.java
+++ src/main/java/io/github/artificialturtill/myriadascension/bloodline/BloodlineApplicationResult.java
@@ -2,7 +2,8 @@ package io.github.artificialturtill.myriadascension.bloodline;
 
 public enum BloodlineApplicationResult {
     INSTALLED,
+    REFINED_SAME_LINEAGE,
     OVERWRITTEN_BY_STRONGER_SAME_LINEAGE,
-    REJECTED_SAME_OR_WEAKER_LINEAGE,
+    REJECTED_WEAKER_OR_IMPURE_SAME_LINEAGE,
     INCOMPATIBLE_LINEAGE_CONFLICT
 }
