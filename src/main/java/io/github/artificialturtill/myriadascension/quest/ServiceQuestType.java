61e45c8e39861040c2487aff717e1052e43e3e30
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 00:40:14 +0200

    feat: add Testudo Clan admission quests (ServiceQuestType.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/quest/ServiceQuestType.java
@@ -0,0 +1,9 @@
+package io.github.artificialturtill.myriadascension.quest;
+
+public enum ServiceQuestType {
+    REPAIR_BUILDING,
+    FETCH_WEAPONS,
+    FETCH_MEDICINE,
+    FETCH_BAKED_GOODS,
+    FETCH_NOODLE_SOUP
+}
