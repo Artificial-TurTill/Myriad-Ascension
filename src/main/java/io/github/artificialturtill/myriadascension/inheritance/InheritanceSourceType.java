750e9458688f92197aa609672c27be20f305ea43
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 00:00:49 +0200

    feat: add cultivation progression foundations (InheritanceSourceType.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/inheritance/InheritanceSourceType.java
@@ -0,0 +1,10 @@
+package io.github.artificialturtill.myriadascension.inheritance;
+
+public enum InheritanceSourceType {
+    MASTER,
+    CLAN,
+    SECT,
+    CULTIVATOR_TOMB,
+    MONUMENT,
+    OTHER_STRUCTURE
+}
