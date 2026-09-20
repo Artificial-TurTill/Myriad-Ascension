eeea70f1c869fffed519fffa1166eb0878e960fc
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-21 01:45:06 +0200

    feat: add Testudo clan institution rules (PrimordialisTestudoFacility.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/clan/PrimordialisTestudoFacility.java
@@ -0,0 +1,13 @@
+package io.github.artificialturtill.myriadascension.clan;
+
+public enum PrimordialisTestudoFacility {
+    CLAN_LEADER_HALL,
+    ELDER_COUNCIL_SPACE,
+    HALL_OF_PUNISHMENT,
+    CLAN_LIBRARY,
+    TRAINING_COURT,
+    MEDITATION_CHAMBER,
+    INHERITANCE_ROOM,
+    DISCIPLE_QUARTERS,
+    SERVANT_QUARTERS
+}
