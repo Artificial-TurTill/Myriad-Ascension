b288973f9174353d763992389280646b86c79140
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-24 12:56:52 +0200

    feat: define cultivation power and abilities (CultivationAbilityState.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/ability/CultivationAbilityState.java
@@ -0,0 +1,7 @@
+package io.github.artificialturtill.myriadascension.ability;
+
+public record CultivationAbilityState(
+        String name,
+        boolean unlocked,
+        String description) {
+}
