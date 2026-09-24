bb2a44d799d9f6d0e43384a639745076a8f14271
Artificial-TurTill <Artificial-TurTill@proton.me>
2026-09-24 12:56:45 +0200

    feat: define cultivation power and abilities (PowerBreakdown.java)

---
+++ src/main/java/io/github/artificialturtill/myriadascension/power/PowerBreakdown.java
@@ -0,0 +1,12 @@
+package io.github.artificialturtill.myriadascension.power;
+
+public record PowerBreakdown(
+        double realmPotential,
+        double currentCombatIndex,
+        double physicalFactor,
+        double energyFactor,
+        double soulFactor,
+        double foundationFactor,
+        double conditionFactor,
+        double battleFactor) {
+}
